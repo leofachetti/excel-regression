6b3d6a3e7c68cfad961e0a3164358d473c3d2fab
Leonardo Fachetti <leofachetti@gmail.com>
2015-09-07 10:44:10 -0300

    Testing validation utils

--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 var regression = require('../lib');
+var validation = require('../lib/utils/validation');
 var expect = require('chai').expect;
 var should = require('chai').should();
 var assert = require('chai').assert;
@@ -13,97 +14,97 @@ var validData = [
 ]
 
 describe('data validation', function () {
-  it('should throw a ReferenceError when attribute is null', function () {
+  it('should throw a TypeError when attribute is null', function () {
     try {
-      regression.caulculate(null);
+      validation.validate(null);
     } catch(e) {
-      expect(e).to.be.an.instanceof(ReferenceError);
+      expect(e).to.be.an.instanceof(TypeError);
       expect(e.message).to.have.string('Attribute `values` must be a array');
       return;
     }
     assert(false, "Should not get here");
   });
 
-  it('should throw a ReferenceError when attribute is a string', function () {
+  it('should throw a TypeError when attribute is a string', function () {
     try {
-      regression.caulculate("");
+      validation.validate("");
     } catch(e) {
-      expect(e).to.be.an.instanceof(ReferenceError);
+      expect(e).to.be.an.instanceof(TypeError);
       expect(e.message).to.have.string('Attribute `values` must be a array');
       return;
     }
     assert(false, "Should not get here");
   });
 
-  it('should throw a ReferenceError when attribute is not set', function () {
+  it('should throw a TypeError when attribute is not set', function () {
     try {
-      regression.caulculate();
+      validation.validate();
     } catch(e) {
-      expect(e).to.be.an.instanceof(ReferenceError);
+      expect(e).to.be.an.instanceof(TypeError);
       expect(e.message).to.have.string('Attribute `values` must be a array');
       return;
     }
     assert(false, "Should not get here");
   });
 
-  it('should throw a ReferenceError when reciving a object', function () {
+  it('should throw a TypeError when reciving a object', function () {
     try {
-      regression.caulculate({});
+      validation.validate({});
     } catch(e) {
-      expect(e).to.be.an.instanceof(ReferenceError);
+      expect(e).to.be.an.instanceof(TypeError);
       expect(e.message).to.have.string('Attribute `values` must be a array');
       return;
     }
     assert(false, "Should not get here");
   });
 
-  it('should throw a ReferenceError when reciving a empty array', function () {
+  it('should throw a TypeError when reciving a empty array', function () {
     try {
-      regression.caulculate([]);
+      validation.validate([]);
     } catch(e) {
-      expect(e).to.be.an.instanceof(ReferenceError);
+      expect(e).to.be.an.instanceof(TypeError);
       expect(e.message).to.have.string('cannot be a empty');
       return;
     }
     assert(false, "Should not get here");
   });
 
-  it('should throw a ReferenceError when reciving a non 2 dimensional array', function () {
+  it('should throw a TypeError when reciving a non 2 dimensional array', function () {
     try {
-      regression.caulculate([1, 1, 3]);
+      validation.validate([1, 1, 3]);
     } catch(e) {
-      expect(e).to.be.an.instanceof(ReferenceError);
+      expect(e).to.be.an.instanceof(TypeError);
       expect(e.message).to.have.string('Attribute `value` must be a array');
       return;
     }
     assert(false, "Should not get here");
   });
 
-  it('should throw a ReferenceError when reciving a non full 2 dimensional array', function () {
+  it('should throw a TypeError when reciving a non full 2 dimensional array', function () {
     try {
-      regression.caulculate([[1, 2], 1, 3]);
+      validation.validate([[1, 2], 1, 3]);
     } catch(e) {
-      expect(e).to.be.an.instanceof(ReferenceError);
+      expect(e).to.be.an.instanceof(TypeError);
       expect(e.message).to.have.string('Attribute `value` must be a array');
       return;
     }
     assert(false, "Should not get here");
   });
 
-  it('should throw a ReferenceError when reciving full 2 dimensional array with numbers of elements diferent than 2', function () {
+  it('should throw a TypeError when reciving full 2 dimensional array with numbers of elements diferent than 2', function () {
     try {
-      regression.caulculate([[1, 2], [1], [3, 2]]);
+      validation.validate([[1, 2], [1], [3, 2]]);
     } catch(e) {
-      expect(e).to.be.an.instanceof(ReferenceError);
+      expect(e).to.be.an.instanceof(TypeError);
       expect(e.message).to.have.string('Attribute `value` must have 2 elements');
       return;
     }
     assert(false, "Should not get here");
   });
 
-  it('should not throw a ReferenceError when reciving a valid attribute', function () {
+  it('should not throw a TypeError when reciving a valid attribute', function () {
     try {
-      regression.caulculate(validData);
+      validation.validate(validData);
     } catch(e) {
       assert(false, "Should not get here");
     }
