43839b0b38e9b72cd4469fa76b8c9dd8f6bc0505
Leonardo Fachetti <leofachetti@gmail.com>
2015-09-22 23:42:04 -0300

    Summary r square

---
+++ test/summary.js
@@ -0,0 +1,19 @@
+var regression = require('../lib');
+var expect = require('chai').expect;
+var should = require('chai').should();
+var assert = require('chai').assert;
+
+var validData = [
+  [0.827106,	32699512],
+  [1.275007,	50550730],
+  [1.435098,	56214886],
+  [1.597657,	63010730],
+  [1.748174,	68767902],
+  [1.904614,	75539606]
+];
+
+describe('summary output', function () {
+  it('should return the r square', function() {
+    expect(0.999629165).to.be.equal(regression.calculate(validData).summary.rSquare);
+  });
+});
