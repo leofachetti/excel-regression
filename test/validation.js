de345d7016feaaf079b617ce718e843a7b6aaf2b
Leonardo Fachetti <leofachetti@gmail.com>
2015-09-28 22:36:56 -0300

    Fixing 'brace-style'

--- test/validation.js
+++ test/validation.js
@@ -12,55 +12,73 @@ var validData = [
 
 describe('data validation', function() {
   it('should throw a TypeError when attribute is null', function() {
-    var func = function() { validation.validate(null); };
+    var func = function() {
+			validation.validate(null);
+		};
     expect(func).to.throw(TypeError);
     expect(func).to.throw(/Attribute `values` must be a array/);
   });
 
   it('should throw a TypeError when attribute is a string', function() {
-    var func = function() { validation.validate(""); };
+    var func = function() {
+			validation.validate("");
+		};
     expect(func).to.throw(TypeError);
     expect(func).to.throw(/Attribute `values` must be a array/);
   });
 
   it('should throw a TypeError when attribute is not set', function() {
-    var func = function() { validation.validate(); };
+    var func = function() {
+			validation.validate();
+		};
     expect(func).to.throw(TypeError);
     expect(func).to.throw(/Attribute `values` must be a array/);
   });
 
   it('should throw a TypeError when reciving a object', function() {
-    var func = function() { validation.validate({}); };
+    var func = function() {
+			validation.validate({});
+		};
     expect(func).to.throw(TypeError);
     expect(func).to.throw(/Attribute `values` must be a array/);
   });
 
   it('should throw a TypeError when reciving a empty array', function() {
-    var func = function() { validation.validate([]); };
+    var func = function() {
+			validation.validate([]);
+		};
     expect(func).to.throw(TypeError);
     expect(func).to.throw(/Attribute `values` cannot be a empty/);
   });
 
   it('should throw a TypeError when reciving a non 2 dimensional array', function() {
-    var func = function() { validation.validate([1, 1, 3]); };
+    var func = function() {
+			validation.validate([1, 1, 3]);
+		};
     expect(func).to.throw(TypeError);
     expect(func).to.throw(/Attribute `value` must be a array/);
   });
 
   it('should throw a TypeError when reciving a non full 2 dimensional array', function() {
-    var func = function() { validation.validate([[1, 2], 1, 3]); };
+    var func = function() {
+			validation.validate([[1, 2], 1, 3]);
+		};
     expect(func).to.throw(TypeError);
     expect(func).to.throw(/Attribute `value` must be a array/);
   });
 
   it('should throw a TypeError when reciving full 2 dimensional array with numbers of elements diferent than 2', function() {
-    var func = function() { validation.validate([[1, 2], [1], [3, 2]]); };
+    var func = function() {
+			validation.validate([[1, 2], [1], [3, 2]]);
+		};
     expect(func).to.throw(TypeError);
     expect(func).to.throw(/Attribute `value` must have 2 elements/);
   });
 
   it('should not throw a TypeError when reciving a valid attribute', function() {
-    var func = function() { validation.validate(validData); };
+    var func = function() {
+			validation.validate(validData);
+		};
     expect(func).not.to.throw(TypeError);
     expect(func).not.to.throw(Error);
   });
